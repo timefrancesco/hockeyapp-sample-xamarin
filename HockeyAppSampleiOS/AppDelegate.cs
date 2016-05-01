50a9d8b38bedf95d826b79623acd6f4dfe545586
Francesco Pretelli <francesco@timelabs.io>
2016-05-02 10:46:40 +1200

    updated to support HockeApp v4.1.0

--- HockeyAppSampleiOS/AppDelegate.cs
+++ HockeyAppSampleiOS/AppDelegate.cs
@@ -9,74 +9,51 @@ using System.IO;
 
 namespace HockeyAppSampleiOS
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the
-	// User Interface of the application, as well as listening (and optionally responding) to
-	// application events from iOS.
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
-	{
-		const string HOCKEYAPP_APPID = "YOUR-HOCKEYAPP-APP-ID";
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        const string HOCKEYAPP_APPID = "YOUR-HOCKEYAPP-APP-ID";
 
-		UINavigationController navController;
-		HomeViewController homeViewController;
-		UIWindow window;
+        UINavigationController navController;
+        HomeViewController homeViewController;
+        UIWindow window;
 
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{			
-			InitHockeyApp();
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {			
+            InitHockeyApp();
 			
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-			homeViewController = new HomeViewController ();
-			navController = new UINavigationController (homeViewController);
-			window.RootViewController = navController;
+            homeViewController = new HomeViewController();
+            navController = new UINavigationController(homeViewController);
+            window.RootViewController = navController;
 
-			window.MakeKeyAndVisible ();
-			return true;
-		}
-		
-		
-		public class CustomCrashDelegate:BITCrashManagerDelegate
-		{
-			//Called at the next restart after a crash, the content of the file will be visible 
-			//in the HockeyApp dashboard under the "Description" tab
-			public override string ApplicationLogForCrashManager (BITCrashManager crashManager)
-			{
-				return File.ReadAllText("temp.log");
-			}
-		}
-		
-		private void InitHockeyApp()
-		{
-			Setup.EnableCustomCrashReporting (() => {
+            window.MakeKeyAndVisible();
+            return true;
+        }
 
-				//Get the shared instance
-				var manager = BITHockeyManager.SharedHockeyManager;
-				BITHockeyManager.SharedHockeyManager.ConfigureWithIdentifier("betaID","liveID",new CustomCrashDelegate());
+        private void InitHockeyApp()
+        {
+            var manager = BITHockeyManager.SharedHockeyManager;       
+            manager.ConfigureWithIdentifier(HOCKEYAPP_APPID,HOCKEYAPP_APPID,new CustomCrashDelegate());
+            manager.CrashManager.CrashManagerStatus = BITCrashManagerStatus.AutoSend;
+            manager.StartManager ();
+            manager.Authenticator.AuthenticateInstallation ();
+        }
+    }
 
-				//Configure it to use our APP_ID
-				manager.Configure (HOCKEYAPP_APPID);
-
-				//manager.CrashManager.Delegate = crashDelegate;
-				manager.CrashManager.CrashManagerStatus = BITCrashManagerStatus.AutoSend;
-				
-				//Start the manager
-				manager.StartManager ();				
-				
-				//Authenticate (there are other authentication options)
-				manager.Authenticator.AuthenticateInstallation ();
-
-				//Rethrow any unhandled .NET exceptions as native iOS 
-				// exceptions so the stack traces appear nicely in HockeyApp
-				AppDomain.CurrentDomain.UnhandledException += (sender, e) => 
-					Setup.ThrowExceptionAsNative(e.ExceptionObject);
-
-				TaskScheduler.UnobservedTaskException += (sender, e) => 
-					Setup.ThrowExceptionAsNative(e.Exception);
-			});
-		}
-		
-		
-	}
+    public class CustomCrashDelegate:BITCrashManagerDelegate
+    {
+        //Called at the next restart after a crash, the content of the file will be visible
+        //in the HockeyApp dashboard under the "Description" tab
+        public override string ApplicationLogForCrashManager(BITCrashManager crashManager)
+        {
+            {
+                return File.ReadAllText("temp.log");
+            }
+        }
+    }
 }
-
